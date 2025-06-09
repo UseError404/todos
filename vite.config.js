9bf5f58cb9bbb7429baa978d4d2d970cad28d786
Osip <52411274+Osip-d25@users.noreply.github.com>
2025-06-09 18:16:07 +0300

    ver.2 login

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/todos/',
   resolve: {
     alias: {
       '@scss': path.resolve(__dirname, './src/shared/styles')
